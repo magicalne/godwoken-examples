aa49013803bc6bd261e4332a3389d3e29532011c
Flouse <flouse@gmail.com>
2021-07-14 00:31:41 +0800

    fixup! Support Testnet concurrent testing

--- packages/polyjuice/scripts/multi-sign-wallet.ts
+++ packages/polyjuice/scripts/multi-sign-wallet.ts
@@ -1,32 +1,17 @@
-import {
-  ContractFactory,
-  Contract,
-  BigNumberish,
-  providers,
-  Overrides,
-  CallOverrides,
-  BigNumber,
-  utils as ethersUtils,
-  Signer,
-  PopulatedTransaction,
-} from "ethers";
-
-import { TransactionSubmitter } from "../TransactionSubmitter";
-import {
-  web3Rpc,
-  networkSuffix,
-  initGwAccountIfNeeded,
-  isGodwoken,
-  polyjuice_config,
-  polyjuiceRPC,
-} from "../common";
-
-import WalletSimple from "../contracts/WalletSimple.sol/WalletSimple.json";
-import MintableToken from "../contracts/MintableToken.sol/MintableToken.json";
-
-import { PolyjuiceWallet, PolyjuiceConfig } from "@polyjuice-provider/ethers";
 import { AbiItems } from "@polyjuice-provider/base/lib/abi";
+import { PolyjuiceConfig, PolyjuiceWallet } from "@polyjuice-provider/ethers";
 import dotenv from "dotenv";
+import {
+  BigNumber, BigNumberish, CallOverrides, Contract, ContractFactory, Overrides, PopulatedTransaction, providers, Signer, utils as ethersUtils
+} from "ethers";
+import {
+  initGwAccountIfNeeded,
+  isGodwoken, networkSuffix, polyjuiceRPC, polyjuice_config as polyjuiceConfig, web3Rpc
+} from "../common";
+import MintableToken from "../contracts/MintableToken.sol/MintableToken.json";
+import WalletSimple from "../contracts/WalletSimple.sol/WalletSimple.json";
+import { TransactionSubmitter } from "../TransactionSubmitter";
+
 dotenv.config();
 
 const PolyjuiceWalletConfig: PolyjuiceConfig = {
@@ -325,8 +310,25 @@ export async function generateSignedTx(
 [
   "1473ec0e7c507de1d5c734a997848a78ee4d30846986d6b1d22002a57ece74ba",
   "f5e9bac200a2eca0b0eead8a327ef3dc148ba10e192d07badad2d195f2488b94",
-  "a443ed1e456f0f23bcdf4f302f599cf77530d594ad896e84b549a04b0ea40c10"
+  "366cbc0fd979df1695613257128ad4a79ba4f11b3cd98048091a190466325581",
+  "d50ef72f0e20ee495153f8b40c135c1675955c006ae75081ba4c824791e25a2a",
+  "a443ed1e456f0f23bcdf4f302f599cf77530d594ad896e84b549a04b0ea40c10",
+  "38c69fca69ac55c50dfae12dcd43c40017dca07050728a65638ddad1d9f8fa04",
+  "2714b4c7cff48976537c27644c3d0ddda12ae880117860bbab94f839375a649f",
+  "775c3f5274046f5759e7663ff36272333c87e823b312fca1df00d53b0c607f19",
+  "66c8f88a0e0363b73106dcffaff896054749d9a7be24cdd17ecd7212635857e1",
+  "481a8a358d7b83c6d7321a5845fef7c7e6f3446abc8bfe0e51d3ae5c36aadcc2",
+  "41d239de35d8d25c7ac66b50f03ef24c821991bb3409f55035a0033b238689ed",
+  "51f55f5a938504330ec567170d60deab291b744ba7e5c264cbfe8d8d35451e80",
+  "e94e8aea22f05e69153fba872c242ec9864929ed14a46fa6a0c423c0bdbeb034",
+  "41767eb58251891d71987874209cb23f7047710313e215585db34f1b8c5ef344",
+  "6fd2c46e497cdbc85166a2d7bc3da2a4dceb0fa57944b2ad01504861bfe614ce",
+  "0fd36b7d7124ee58b89a2c09067ca3fcdac398901bdfcafb442aafd51776199e",
+  "33cfde51bd1946f405967eb7158d43ba2cfe72c596c4eae9bb566739ad2c4e39",
+  "d328f35ca8c46ac4628184c31c4d43bb02ed1cadd4e33a9c6214f51ef2c6b0b1",
+  "5e6ab6b72b5b436ba36bfc3d410f863e9ac9bcb728d88f054b10ea1825c3b5d7",
+  "fd3b6c8b07ed130b78f924523189db5bfebb3cb42f212ec0db9af7e39c21649b",
 ].forEach(privKey => {
-  testMultiSignWallet(new PolyjuiceWallet(privKey, polyjuice_config, polyjuiceRPC))
+  testMultiSignWallet(new PolyjuiceWallet(privKey, polyjuiceConfig, polyjuiceRPC))
     .catch(console.error);
 });
