2b989b54cbfd195c1cac06a51eb2a45a144b80d7
Flouse <flouse@gmail.com>
2022-02-19 10:17:27 +0000

    Fix tools/packages/tools/configs/generate-envfile.js

--- packages/tools/configs/generate-envfile.js
+++ packages/tools/configs/generate-envfile.js
@@ -10,14 +10,27 @@ const { stringify } = require('envfile');
 const path = require("path");
 const fs = require('fs');
 
+// read script_type_hash from scripts-deploy-result.json
+const scriptsDeployResultPath
+  = path.resolve(__dirname, "../../../../kicker/workspace/deploy/scripts-deploy-result.json");
+const scriptsDeployResult = require(scriptsDeployResultPath);
+const ETH_ACCOUNT_LOCK_CODE_HASH
+  = scriptsDeployResult.eth_account_lock.script_type_hash;
+const POLYJUICE_TYPE_SCRIPT_HASH
+  = scriptsDeployResult.polyjuice_validator.script_type_hash;
+
 // read godwoken-config.toml from godwoken-kicker workspace
 const godwokenConfigPath = process.env.GW_CONFIG_PATH || path.resolve(
   __dirname, "../../../../kicker/workspace/config.toml");
 const tomlStr = fs.readFileSync(godwokenConfigPath);
-const configJson = require('@iarna/toml').parse(tomlStr);
-const ROLLUP_TYPE_HASH = configJson.genesis.rollup_type_hash;
-const ETH_ACCOUNT_LOCK_CODE_HASH = configJson.web3_indexer.eth_account_lock_hash;
-const POLYJUICE_CONTRACT_CODE_HASH = configJson.web3_indexer.polyjuice_script_type_hash;
+const gwConfigJson = require('@iarna/toml').parse(tomlStr);
+const ROLLUP_TYPE_HASH = gwConfigJson.genesis.rollup_type_hash;
+
+// optional: read configs from ../../../../kicker/workspace/indexer-config.toml
+// const indexerConfigPath = process.env.WEB3_INDEXER_CONFIG_PATH || path.resolve(
+  // __dirname, "../../../../kicker/workspace/indexer-config.toml");
+// const envStr = fs.readFileSync(indexerConfigPath);
+// const configJson = parse(envStr);
 
 const CREATOR_ACCOUNT_ID = 3;
 const DEPLOYER_PRIVATE_KEY = "6cd5e7be2f6504aa5ae7c0c04178d8f47b7cfc63b71d95d9e6282f5b090431bf";
@@ -29,7 +42,7 @@ const NETWORK_SUFFIX = "gwk-devnet"
 const envfileJson = {
   ROLLUP_TYPE_HASH,
   ETH_ACCOUNT_LOCK_CODE_HASH,
-  POLYJUICE_CONTRACT_CODE_HASH,
+  POLYJUICE_CONTRACT_CODE_HASH: POLYJUICE_TYPE_SCRIPT_HASH,
   CREATOR_ACCOUNT_ID,
   DEPLOYER_PRIVATE_KEY,
   SIGNER_PRIVATE_KEYS,
@@ -57,13 +70,13 @@ function updateLendingContractConfig() {
   lendingContractsConfigs.deployer = "0x6cd5e7be2f6504aa5ae7c0c04178d8f47b7cfc63b71d95d9e6282f5b090431bf";
   lendingContractsConfigs.nervos.rollup_type_hash = ROLLUP_TYPE_HASH;
   lendingContractsConfigs.nervos.rollup_type_script = {
-    code_hash: configJson.chain.rollup_type_script.code_hash,
-    hash_type: configJson.chain.rollup_type_script.hash_type,
-    args: configJson.chain.rollup_type_script.args
+    code_hash: gwConfigJson.chain.rollup_type_script.code_hash,
+    hash_type: gwConfigJson.chain.rollup_type_script.hash_type,
+    args: gwConfigJson.chain.rollup_type_script.args
   };
   lendingContractsConfigs.nervos.eth_account_lock_hash = ETH_ACCOUNT_LOCK_CODE_HASH;
   lendingContractsConfigs.nervos.deposit_lock_script_type_hash
-    = configJson.genesis.rollup_config.deposit_script_type_hash;
+    = gwConfigJson.genesis.rollup_config.deposit_script_type_hash;
   console.debug("\n> testcases/lending-contracts/config.json:",
     lendingContractsConfigs);
   fs.writeFileSync(
