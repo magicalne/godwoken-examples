cf467dca7959a81abfb2fb68d8c3419ca4f40a6f
magicalne <magicalne@gmail.com>
2022-03-16 18:06:55 +0800

    feat: deposit ckb from faucet

--- packages/tools/src/benchmark/ckb-testnet-faucet.ts
+++ packages/tools/src/benchmark/ckb-testnet-faucet.ts
@@ -1,34 +1,38 @@
-import puppeteer from "puppeteer";
 import { initConfig } from "../account/common";
 import { privateKeyToCkbAddress } from "../modules/utils";
 import { privKeys } from "./accounts";
+import fetch from "node-fetch";
 
 (async () => {
   // init testnet LUMOS_CONFIG
   initConfig();
-
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto('https://faucet.nervos.org', {
-    waitUntil: 'networkidle2',
-  });
-
-  for await (const privKey of privKeys.reverse()) {
-    const ckbAddress = privateKeyToCkbAddress(privKey);
-
-    // Enter one aggron wallet address
-    let input = await page.$('input[name=address_hash]');
-    if (!input) continue;
-    await input.click({ clickCount: 3 });
-    await input.type(ckbAddress,
-      { delay: 50 } // Types slower, like a user
-    );
-
-    // Trigger the `Claim` Button
-    await page.click('button', { delay: 200 });
-
-    console.log(`Claimed testnet 10000 CKB from the faucet for ${ckbAddress}`);
+  for (const pk of privKeys.reverse().slice(0, 100)) {
+    console.log(pk);
+    const addr = privateKeyToCkbAddress(pk);
+    console.log(addr);
+    let res = await fetch("https://faucet.nervos.org/claim_events", {
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6",
+        "content-type": "application/json",
+        "sec-ch-ua":
+          '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Linux"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-csrf-token":
+          "nVPJ5tZ5/WZZVhO7pTHd6swLIVTUpUQem8cHPgAY1nLYSJxLKJlgRnW4wq+SlnCX8vJ9JghMchxqeMbCRN9d5w==",
+      },
+      body: `{\"claim_event\":{\"address_hash\":\"${addr}\"}}`,
+      method: "POST",
+    });
+    console.log(res);
   }
-
-  await browser.close();
+  // const browser = await puppeteer.launch({ headless: true });
+  // const page = await browser.newPage();
+  // await page.goto('https://faucet.nervos.org', {
+  // waitUntil: 'networkidle2',
+  // });
 })();
