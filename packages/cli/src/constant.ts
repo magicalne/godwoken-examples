74a1893712e294f14716fc17d59244bedf899ea3
keroro <keroroxx520@gmail.com>
2022-05-13 18:07:45 +0800

    feat: define godwoken constants

--- packages/cli/src/constant.ts
+++ packages/cli/src/constant.ts
@@ -1,3 +1,6 @@
-export const FEE = BigInt(100000); // TODO find a good fee
-export const MINIMAL_CKB_CELL_CAPACITY = BigInt(6400000000 * 2); // TODO find a good capacity
-export const MINIMUM_DEPOSIT_CAPACITY = BigInt(500) * BigInt(100000000); // TODO find a good deposit capacity
+export const L1_FEE = BigInt(100000); // TODO find a good fee
+export const SHANNON_PER_CKB = BigInt(100000000);
+export const MINIMAL_CKB_CELL_CAPACITY =
+  BigInt(2) * BigInt(75) * SHANNON_PER_CKB; // TODO find a good capacity
+export const MINIMAL_DEPOSIT_CAPACITY = BigInt(242 * 2) * SHANNON_PER_CKB; // TODO find a good deposit capacity
+export const MINIMAL_WITHDRAWAL_CAPACITY = BigInt(242 * 2) * SHANNON_PER_CKB;
